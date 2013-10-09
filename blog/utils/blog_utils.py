b934232cbe139512fa162bec525c0914a6208a41
Ryan Kuczka <ryan.kuczka@gmail.com>
2013-10-08 22:20:51 -0400

    add publish date to archive

--- blog/utils/blog_utils.py
+++ blog/utils/blog_utils.py
@@ -7,15 +7,14 @@ def build_blog_date_dict():
         .order_by('publish_date')
     entry_dict = SortedDict()
     for e in entries:
-        menu = (e.title, e.get_absolute_url())
         month = '{:0>2}'.format(e.publish_date.month)
         month_name = e.publish_date.strftime('%B')
         if e.publish_date.year not in entry_dict:
             entry_dict.insert(0, e.publish_date.year, SortedDict(
-                (((month, month_name), [menu]),)
+                (((month, month_name), [e]),)
             ))
         elif (month, month_name) not in entry_dict[e.publish_date.year]:
-            entry_dict[e.publish_date.year][(month, month_name)] = [menu]
+            entry_dict[e.publish_date.year][(month, month_name)] = [e]
         else:
-            entry_dict[e.publish_date.year][(month, month_name)].append(menu)
+            entry_dict[e.publish_date.year][(month, month_name)].append(e)
     return entry_dict
